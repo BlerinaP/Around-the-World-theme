a98ed0fe59dd2e4270b245dad2118fe5208e0f81
BlerinaP <blerina1.p@gmail.com>
2019-11-09 12:17:11 +0100

    Adding polariod effect with CSS3 to our images.

--- page-aboutus.php
+++ page-aboutus.php
@@ -8,7 +8,7 @@ get_header(); ?>
     <!-- section -->
     <section>
         <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-        <h2><?php the_title();?></h2>
+        <h2><span><?php the_title();?></span></h2>
 
             <!-- article -->
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
