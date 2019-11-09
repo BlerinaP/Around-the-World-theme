a6060846baf35b13b6d208e5cd7a59b6442656e1
BlerinaP <blerina1.p@gmail.com>
2019-11-09 13:38:26 +0100

    Creating a page for tours, writing a custom loop to show our Tours

---
+++ page-tours.php
@@ -0,0 +1,51 @@
+<?php
+/*
+  * Template Name: Tours
+ */
+get_header(); ?>
+
+<main role="main">
+    <!-- section -->
+    <section>
+
+
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+            <h2><span><?php the_title();?></span></h2>
+
+            <!-- article -->
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <?php $args = array(
+                    'post_type' => 'tour',
+                    'posts_per_page' => -1,
+                    'order' => 'ASC',
+                    'orderby'=> 'title',
+                ); ?>
+                <?php $tours = new WP_Query($args); while($tours->have_posts()): $tours->the_post(); ?>
+                    <h3><?php the_title(); ?></h3>
+                <?php endwhile; wp_reset_postdata();?>
+
+                <br class="clear">
+
+                <?php edit_post_link(); ?>
+
+            </article>
+            <!-- /article -->
+
+        <?php endwhile; ?>
+
+        <?php else: ?>
+
+            <!-- article -->
+            <article>
+
+                <h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+            </article>
+            <!-- /article -->
+
+        <?php endif; ?>
+
+    </section>
+    <!-- /section -->
+</main>
+<?php get_footer(); ?>
