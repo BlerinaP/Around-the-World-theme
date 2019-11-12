9bece29843fa0a29d1c08895ef55626b187c584c
BlerinaP <blerina1.p@gmail.com>
2019-11-12 12:16:45 +0100

    Adding share buttons in single.php

--- single.php
+++ single.php
@@ -18,6 +18,14 @@
                     <span class="date"><strong><?php _e( 'Published: ', 'html5blank' ); ?></strong><?php the_time('F j, Y'); ?></span><br/>
                     <span class="author"><strong><?php _e( 'by: ', 'html5blank' );?></strong><?php the_author_posts_link(); ?></span>
                     <p><strong><?php _e( 'Category: ', 'html5blank');?> </strong><?php the_category(', '); // Separated by commas ?></p>
+
+                    <div class="share-button">
+                        <p>Share this with your friends:</p>
+                      <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5dca90a955d3baab"></script>
+                       <div class="addthis_inline_share_toolbox_bcs1"></div>
+
+                    </div>
+
                 </div>
                 <div class="grid2-3">
                     <?php the_content();?>
