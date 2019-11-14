a0f35037311fd6f9cb1d1975eb96d890f4a618a7
BlerinaP <blerina1.p@gmail.com>
2019-11-14 12:00:29 +0100

    Writing the loop to show testimonials in Home page

--- front-page.php
+++ front-page.php
@@ -39,34 +39,63 @@
             <?php endwhile; wp_reset_postdata();?>
         </ul>
     </section>
+
     <div class="bottom-content">
         <section class="latest-entries grid2-3">
             <h2><span>Travel Tips From our Blog</span></h2>
 
             <ul>
-            <?php $args = array(
-                    'post_type' => 'post',
-                    'posts_per_page' => 2
-            );
-              $tips = new WP_Query($args);
-              while($tips->have_posts()): $tips->the_post();
-            ?>
-              <li class="clear">
-                  <div class="imagetips grid1-3">
-                      <?php the_post_thumbnail('medium');?>
-                  </div><!--imagetips-->
+                <?php $args = array(
+                        'post_type' => 'post',
+                        'posts_per_page' => 2
+                );
+                  $tips = new WP_Query($args);
+                  while($tips->have_posts()): $tips->the_post();
+                ?>
+                  <li class="clear">
+                      <div class="imagetips grid1-3">
+                          <?php the_post_thumbnail('medium');?>
+                      </div><!--imagetips-->
 
-                  <div class="content-tip grid2-3 omega">
-                      <h3><?php the_title();?></h3>
-                      <?php html5wp_excerpt('html5wp_index'); ?>
-                  </div>
-              </li>
-                <?php endwhile; wp_reset_postdata(); ?>
+                      <div class="content-tip grid2-3 omega">
+                          <h3><?php the_title();?></h3>
+                          <?php html5wp_excerpt('html5wp_index'); ?>
+                      </div>
+                  </li>
+                    <?php endwhile; wp_reset_postdata(); ?>
             </ul>
         </section>
+
         <section class="latest-testimonials grid1-3">
             <h2><span>Testimonials</span></h2>
-        </section>
+            <?php $args = array(
+                'post_type' => 'testimonial',
+                'posts_per_page' => 2,
+                'orderby' => 'date',
+                'order' => 'DESC',
+            ); ?>
+            <ul>
+                <?php $testimonial = new WP_Query($args); while($testimonial->have_posts()): $testimonial->the_post(); ?>
+
+                    <li>
+                        <div class="testimonial-content">
+                            <?php html5wp_excerpt('html5wp_custom_post'); ?>
+
+                            <div class="from-testimonial">
+                                <?php $name = get_field('person_name'); ?>
+                                <?php $from = get_field('from'); ?>
+                                <p><strong><?php echo $name . ', ' . $from  ?></strong></p>
+                            </div>
+                        </div>
+
+                    </li>
+                <?php endwhile; wp_reset_postdata();  ?>
+
+                <a href="<?php echo get_permalink(13); ?>">View All</a>
+            </ul>
+
+
+        </section> <!--.latest-testimonials -->
     </div>
 
 <?php get_footer(); ?>
