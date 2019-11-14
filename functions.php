571bfcdf01065c46d005ef310141970d0b90e00b
BlerinaP <blerina1.p@gmail.com>
2019-11-14 11:08:22 +0100

    Printing latest tours in front page.

--- functions.php
+++ functions.php
@@ -13,25 +13,59 @@ function slider() {
     $args = array(
       'post_type' => 'slider',
       'posts_per_page' => 5,
-      'order' => 'DESC',
+      'order' => 'ASC',
       'orderby' => 'date'
     );
     $slider = new WP_Query($args);
     echo "<ul class='slider'>";
     while($slider->have_posts()): $slider->the_post();
-    echo "<li>";
+        echo "<li>";
         $link = get_field('link');
         echo "<a href='$link'>";
-        the_post_thumbnail('slider');
-    echo "</li>";
+          the_post_thumbnail('slider');
+        echo "</a>";
+        echo "</li>";
     endwhile; wp_reset_postdata();
     echo "</ul>";
 }
 add_action('slider_index', 'slider');
 
+/**** RESPONSIVE IMAGES **/
+/**
+ * Create function my_responsive_thumbnail() to output responsive featured image
+ * Function can be called from within the loop of any template file using
+ * my_responsive_thumbnail(get_the_ID());
+ */
+
+function my_responsive_thumbnail($post_id){
+    // Get the featured image ID
+    $attachment_id = get_post_thumbnail_id($post_id);
+
+    // Get the info for each image size including the original (full)
+    $thumb_large    = wp_get_attachment_image_src($attachment_id, 'tourFront');
+    $thumb_medium   = wp_get_attachment_image_src($attachment_id, 'featuredTour');
+
+    // Create array containing each image size + the alt tag
+    $thumb_data = array(
+        'thumb_large'    => $thumb_large[0],
+        'thumb_medium'   => $thumb_medium[0],
+    );
+
+    // Echo out <picture> element based on code from above
+    echo '<picture>';
+    echo '<!--[if IE 9]><video style="display: none;"><![endif]-->'; // Fallback for IE9
+    echo '<source srcset="' . $thumb_data['thumb_large'] . ', ' . $thumb_data['thumb_large'] . ' " media="(min-width: 800px)">';
+    echo '<source srcset="' . $thumb_data['thumb_medium'] . ', ' . $thumb_data['thumb_medium'] . ' ">';
+    echo '<!--[if IE 9]></video><![endif]-->'; // Fallback for IE9
+    echo '<img srcset="' . $thumb_data['thumb_large'] . ', ' . $thumb_data['thumb_large'] . '">';
+    echo '</picture>';
+}
+
+
 /*------------------------------------*\
 	Theme Support
 \*------------------------------------*/
+
 
 if (!isset($content_width))
 {
@@ -50,6 +84,7 @@ if (function_exists('add_theme_support'))
     add_image_size('blog-images', 1280, 444, true); // Blog images
     add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
     add_image_size('slider', 1500, 500, true); //Slider
+    add_image_size('tourFront', 430, 300, true); //tourfront
 
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
     add_theme_support('custom-background', array(
@@ -127,6 +162,9 @@ function html5blank_header_scripts()
 
         wp_register_script('bxslider', get_template_directory_uri() . '/js/jquery.bxslider.js', array(), '4.2.12'); // bxslider
         wp_enqueue_script('bxslider'); // Enqueue it!
+
+        wp_register_script('picturefill', get_template_directory_uri() . '/js/picturefill.js', array(), '3.0.2'); // picturefill
+        wp_enqueue_script('picturefill'); // Enqueue it!
     }
 }
 
