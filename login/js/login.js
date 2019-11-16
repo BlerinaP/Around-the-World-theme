45d4c442de4e362bb50c64b18db207765d31bde9
BlerinaP <blerina1.p@gmail.com>
2019-11-16 12:39:31 +0100

    Adding the Jquery Vegas Script to our project, and transition for the images.

--- login/js/login.js
+++ login/js/login.js
@@ -1,3 +1,11 @@
 jQuery(function($){
-   console.log(login_images.theme_path);
+   $('body').vegas({
+      slides: [
+         { src: login_images.theme_path + "1.jpg" },
+         { src: login_images.theme_path + "2.jpg" },
+         { src: login_images.theme_path + "3.jpg" },
+      ],
+      overlay: login_images.theme_path + 'overlays/05.png',
+      transition: ['fade', 'zoomIn2', 'swirlLeft']
+   })
 });
